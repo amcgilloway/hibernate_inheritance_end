08d6116b9b8e78e6f81bda8a6257f70f0f2eebf3
Alastair McGilloway <alastairmcgilloway@hotmail.com>
2018-02-18 13:51:14 +0000

    Draft end code for inheritance:

---
+++ src/main/java/Runner.java
@@ -0,0 +1,27 @@
+import db.DBHelper;
+import models.Department;
+import models.Engineer;
+import models.Manager;
+
+public class Runner {
+
+    public static void main(String[] args) {
+        DBHelper.deleteAll("Engineer");
+        DBHelper.deleteAll("Employee");
+        DBHelper.deleteAll("Manager");
+        DBHelper.deleteAll("Department");
+
+
+        Department department = new Department("Hr");
+        DBHelper.save(department);
+        Manager manager = new Manager("Peter", "Griffin", 40000,department, 100000 );
+        DBHelper.save(manager);
+        Engineer engineer1 = new Engineer("Lois", "Griffin", 29000, department);
+        DBHelper.save(engineer1);
+        Engineer engineer2 = new Engineer("Stewie", "Griffin", 27000, department);
+        DBHelper.save(engineer2);
+
+        Engineer found = DBHelper.find(engineer1.getId(), "Engineer");
+        System.out.println();
+    }
+}
---
+++ src/main/java/db/DBHelper.java
@@ -0,0 +1,111 @@
+package db;
+
+import models.Manager;
+import org.hibernate.HibernateException;
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import java.util.List;
+
+public class DBHelper {
+
+    private static Transaction transaction;
+    private static Session session;
+
+    public static void save(Object object) {
+
+        session = HibernateUtil.getSessionFactory().openSession();
+        try {
+            transaction = session.beginTransaction();
+            session.save(object);
+            transaction.commit();
+        } catch (HibernateException e) {
+            transaction.rollback();
+            e.printStackTrace();
+        } finally {
+            session.close();
+        }
+    }
+
+    public static void deleteAll(String className){
+        session = HibernateUtil.getSessionFactory().openSession();
+        try {
+            transaction = session.beginTransaction();
+            String hql ="delete from " + className;
+            Query query = session.createQuery(hql);
+            query.executeUpdate();
+        } catch (HibernateException e){
+            transaction.rollback();
+            e.printStackTrace();
+        } finally {
+            session.close();
+        }
+    }
+
+    public static void update(Object object){
+        session = HibernateUtil.getSessionFactory().openSession();
+        try {
+            transaction = session.beginTransaction();
+            session.update(object);
+            transaction.commit();
+        } catch (HibernateException e) {
+            transaction.rollback();
+            e.printStackTrace();
+        } finally {
+            session.close();
+        }
+    }
+
+    public static void delete(Object object){
+        session = HibernateUtil.getSessionFactory().openSession();
+        try {
+            transaction = session.beginTransaction();
+            session.delete(object);
+            transaction.commit();
+        } catch (HibernateException e) {
+            transaction.rollback();
+            e.printStackTrace();
+        } finally {
+            session.close();
+        }
+    }
+
+    public static <T> List<T> getAll(String className){
+        session = HibernateUtil.getSessionFactory().openSession();
+        List<T> results = null;
+        try {
+            transaction = session.beginTransaction();
+            String hql = "from " + className;
+            Query query = session.createQuery(hql);
+            results = query.list();
+            transaction.commit();
+        } catch (HibernateException e) {
+            transaction.rollback();
+            e.printStackTrace();
+        } finally {
+            session.close();
+        }
+        return results;
+    }
+
+    public static <T> T find(int id, String className){
+        session = HibernateUtil.getSessionFactory().openSession();
+        T result = null;
+        try {
+            transaction = session.beginTransaction();
+            String hql = "from " + className + " where id = :id";
+            Query query = session.createQuery(hql);
+            query.setInteger("id", id);
+            result = (T)query.uniqueResult();
+            transaction.commit();
+        } catch (HibernateException e) {
+            transaction.rollback();
+            e.printStackTrace();
+        } finally {
+            session.close();
+        }
+        System.out.println(result);
+        return result;
+    }
+}
